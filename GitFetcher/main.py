34c32568ec9c58ea211d00e44732687a5bec02a6
admin404 <neterror404@gmail.com>
2022-05-03 18:52:16 +0300

    update

--- GitFetcher/main.py
+++ GitFetcher/main.py
@@ -1,4 +1,16 @@
 import os
 
+def find_dirs():
+	# dir_path = os.path.dirname(os.path.realpath(__file__))
+	dir_path = os.path.dirname("C:\\")
+
+	for root, dirs, files, in os.walk(dir_path):
+		for file in files:
+			if file.endswith('dirs.txt'):
+				print (root + '\\' + str(file))
+
 def main():
-    print("Hello World")
+	find_dirs()
+
+if __name__ == "__main__":
+	main()
